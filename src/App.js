c6b298b0cb9956dc32eb7a16bd499ce081763d9f
Joseph Breidinger <joseph.breidinger@gmail.com>
2025-12-06 23:22:27 -0500

    connected Register to server route - currently routing to sign in, instead of home

--- src/App.js
+++ src/App.js
@@ -58,8 +58,27 @@ class App extends Component {
       box: {},
       route: "signin",
       isSignedIn: false,
+      user: {
+        id: "",
+        name: "",
+        email: "",
+        entries: 0,
+        joined: "",
+      },
     };
   }
+
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined,
+      },
+    });
+  };
 
   componentDidMount() {
     fetch("http://localhost:3000/")
@@ -157,7 +176,10 @@ class App extends Component {
         ) : this.state.route === "signin" ? (
           <SignIn onRouteChange={this.onRouteChange} />
         ) : (
-          <Register onRouteChange={this.onRouteChange} />
+          <Register
+            loadUser={this.loadUser}
+            onRouteChange={this.onRouteChange}
+          />
         )}
       </div>
     );
