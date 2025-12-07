23f2feff4636a5bea2506cadc94138db48d2c830
Joseph Breidinger <joseph.breidinger@gmail.com>
2025-12-06 22:55:02 -0500

    connected SignIn to server route

--- src/App.js
+++ src/App.js
@@ -61,6 +61,13 @@ class App extends Component {
     };
   }
 
+  componentDidMount() {
+    fetch("http://localhost:3000/")
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error("Error fetching from server:", error));
+  }
+
   // calculateFaceLocation = (data) => {
   //   const clarifaiFace =
   //     data.outputs[0].data.regions[0].region_info.bounding_box;
